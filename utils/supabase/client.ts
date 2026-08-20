70dc172d63387700320afb418f8267ec41314feb
mmajor805 <marcelomirandajr805@gmail.com>
2026-08-20 18:54:24 -0300

    Update client.ts

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,15 +1,15 @@
 import { createBrowserClient } from "@supabase/ssr";
 
-const supabaseUrl =
-  process.env.NEXT_PUBLIC_SUPABASE_URL ||
-  "https://vdypyryzlbpkktqticns.supabase.co";
+export const createClient = () => {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 
-const supabaseKey =
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY ||
-  "sb_publishable_ksdutK44h8AFSwcdFHg4TA_3kR15zZY";
+  if (!supabaseUrl || !supabaseKey) {
+    throw new Error("Supabase environment variables are missing.");
+  }
 
-export const createClient = () =>
-  createBrowserClient(
+  return createBrowserClient(
     supabaseUrl,
     supabaseKey
   );
+};
