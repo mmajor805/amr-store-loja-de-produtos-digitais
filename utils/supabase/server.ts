06bfb72f465a8a417011d71f5d81242abd01b140
mmajor805 <marcelomirandajr805@gmail.com>
2026-08-20 19:24:43 -0300

    Update server.ts

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,26 +1,36 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
+const supabaseUrl =
+  "https://vdypyryzlbpkktqticns.supabase.co";
+
+const supabaseKey =
+  "sb_publishable_mJOeqC6Kv5Yz1C8dqObQQw_xLqySUQ0";
 
 export async function createClient() {
   const cookieStore = await cookies();
 
-  return createServerClient(supabaseUrl, supabaseKey, {
-    cookies: {
-      getAll() {
-        return cookieStore.getAll();
+  return createServerClient(
+    supabaseUrl,
+    supabaseKey,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(
+              ({ name, value, options }) => {
+                cookieStore.set(name, value, options);
+              }
+            );
+          } catch {
+            // Pode ser ignorado em Server Components.
+          }
+        },
       },
-      setAll(cookiesToSet) {
-        try {
-          cookiesToSet.forEach(({ name, value, options }) =>
-            cookieStore.set(name, value, options)
-          );
-        } catch {
-          // Pode ser ignorado em Server Components.
-        }
-      },
-    },
-  });
+    }
+  );
 }
