78bfc67e735a937d28f60e2f1595613622c757e8
mmajor805 <marcelomirandajr805@gmail.com>
2026-08-20 20:21:54 -0300

    Update middleware.ts

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -2,21 +2,13 @@ import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function updateSession(request: NextRequest) {
-  let response = NextResponse.next({
+  let supabaseResponse = NextResponse.next({
     request,
   });
 
-  const supabaseUrl =
-    process.env.NEXT_PUBLIC_SUPABASE_URL ||
-    "https://vdypyryzlbpkktqticns.supabase.co";
-
-  const supabaseKey =
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY ||
-    "sb_publishable_mJOeqC6Kv5Yz1C8dqObQQw_xLqySUQ0";
-
   const supabase = createServerClient(
-    supabaseUrl,
-    supabaseKey,
+    "https://vdypyryzlbpkktqticns.supabase.co",
+    "sb_publishable_mJOeqC6Kv5Yz1C8dqObQQw_xLqySUQ0",
     {
       cookies: {
         getAll() {
@@ -28,13 +20,19 @@ export async function updateSession(request: NextRequest) {
             request.cookies.set(name, value);
           });
 
-          response = NextResponse.next({
+          supabaseResponse = NextResponse.next({
             request,
           });
 
-          cookiesToSet.forEach(({ name, value, options }) => {
-            response.cookies.set(name, value, options);
-          });
+          cookiesToSet.forEach(
+            ({ name, value, options }) => {
+              supabaseResponse.cookies.set(
+                name,
+                value,
+                options
+              );
+            }
+          );
         },
       },
     }
@@ -42,5 +40,5 @@ export async function updateSession(request: NextRequest) {
 
   await supabase.auth.getUser();
 
-  return response;
+  return supabaseResponse;
 }
