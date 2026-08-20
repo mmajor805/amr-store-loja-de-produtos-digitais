dc34b6955e792f033cf02d6d9f49b83f58239f53
mmajor805 <marcelomirandajr805@gmail.com>
2026-08-20 18:26:58 -0300

    Update client.ts

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,7 +1,15 @@
 import { createBrowserClient } from "@supabase/ssr";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  "https://vdypyryzlbpkktqticns.supabase.co";
+
+const supabaseKey =
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY ||
+  "sb_publishable_ksdutK44h8AFSwcdFHg4TA_3kR15zZY";
 
 export const createClient = () =>
-  createBrowserClient(supabaseUrl, supabaseKey);
+  createBrowserClient(
+    supabaseUrl,
+    supabaseKey
+  );
