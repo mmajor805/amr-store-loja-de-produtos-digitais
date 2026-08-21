959fa32c6bdf23998b98da586e3b1e128a7d8edf
mmajor805 <marcelomirandajr805@gmail.com>
2026-08-20 21:28:02 -0300

    Update page.tsx

--- app/produto/[id]/page.tsx
+++ app/produto/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import { createClient } from "../utils/supabase/client";
+import { createClient } from "../../../../utils/supabase/client";
 
 type Product = {
   id: number;
