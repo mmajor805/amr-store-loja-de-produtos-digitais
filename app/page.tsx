7e70cf1d52462e0ba186696e51049ae89c0bcd3e
mmajor805 <marcelomirandajr805@gmail.com>
2026-08-22 00:02:10 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -324,7 +324,7 @@ export default function Home() {
           {!loading &&
             filteredProducts.length > 0 && (
 
-              <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+              <div className="grid grid-cols-2 gap-3 sm:gap-6 lg:grid-cols-3">
 
                 {filteredProducts.map((product) => {
 
